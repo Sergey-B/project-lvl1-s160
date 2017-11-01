80cc990e429a63552fbbbe3d9d0f7326ed856f26
Sergey Belov <belovtorzhok@gmail.com>
2017-11-01 11:14:39 +0300

    add arithmetic progression game

---
+++ src/games/progression.js
@@ -0,0 +1,26 @@
+import { playGame, randomNum } from '..';
+
+const randomProgression = (length, start = randomNum(10, 100), diff = randomNum(2, 9)) => {
+  const array = [];
+
+  for (let i = 1; i <= length; i += 1) {
+    const el = start + ((i - 1) * diff);
+    array.push(el);
+  }
+
+  return array;
+};
+
+const rules = 'What number is missing in this progression?';
+const makeQuestion = () => {
+  const progression = randomProgression(10);
+  const randomIndex = randomNum(0, 9);
+  const answer = progression[randomIndex];
+
+  progression[randomIndex] = '..';
+
+  return { question: progression.join(' '), answer };
+};
+const checkAnswer = (answer, solution) => parseInt(answer, 10) === parseInt(solution, 10);
+
+export default () => { playGame(rules, makeQuestion, checkAnswer); };
