ce4694d4514fdfcfdd41f922268ec5dce7137159
Sergey Belov <belovtorzhok@gmail.com>
2017-11-01 10:22:28 +0300

    add digit balancing game

---
+++ src/bin/brain-balance.js
@@ -0,0 +1,4 @@
+#! /usr/bin/env node
+import balance from '../games/balance';
+
+balance();
---
+++ src/games/balance.js
@@ -0,0 +1,26 @@
+import { playGame, randomNum } from '..';
+
+const balanceNum = (num) => {
+  const arrayInt = num.toString().split('').map(n => parseInt(n, 10)).sort();
+
+  if (Math.max(...arrayInt) - Math.min(...arrayInt) <= 1) {
+    return arrayInt.join('');
+  }
+
+  const indexMax = arrayInt.indexOf(Math.max(...arrayInt));
+  const indexMin = arrayInt.indexOf(Math.min(...arrayInt));
+
+  arrayInt[indexMax] -= 1;
+  arrayInt[indexMin] += 1;
+
+  return balanceNum(parseInt(arrayInt.join(''), 10));
+};
+
+const rules = 'Balance the given number.';
+const makeQuestion = () => {
+  const num = randomNum(1, 5000);
+  return { question: `${num}`, answer: balanceNum(num) };
+};
+const checkAnswer = (answer, solution) => parseInt(answer, 10) === parseInt(solution, 10);
+
+export default () => { playGame(rules, makeQuestion, checkAnswer); };
